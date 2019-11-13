3ab6bb1620125c6146f7a9fa9e6405292b57f374
NewbieCoder99 <fajarpunya99@gmail.com>
2019-11-13 20:15:53 +0700

    latest update

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,18 +1,17 @@
-$('#sidebarCollapse').on('click', function() {
-    $('#sidebar').toggleClass('active');
-});
-
-$(document).ready(function() {
-
-	$(function() {
-		$( ".draggable" ).draggable();
-	});
-
-	$( function() {
-		$( ".accordion" ).accordion({
-		  collapsible: true
-		});
-	});
-
-	$('[data-toggle="popover"]').popover();
+$(document).ready(function(){
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.show").each(function(){
+        $(this).parent(".card").find(".svg-inline--fa").removeClass("fa-plus").addClass("fa-minus");
+    });
+    
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+        $(this).prev(".card-header").find(".svg-inline--fa").removeClass("fa-plus").addClass("fa-minus");
+        $(this).parent(".card").find(".card-header").addClass("bg-white border-bottom-0");
+        $(this).parent(".card").addClass("border-success");
+    }).on('hide.bs.collapse', function(){
+        $(this).prev(".card-header").find(".svg-inline--fa").removeClass("fa-minus").addClass("fa-plus");
+        $(this).parent(".card").find(".card-header").removeClass("bg-white border-bottom-0");
+        $(this).parent(".card").removeClass("border-success");
+    });
 });
